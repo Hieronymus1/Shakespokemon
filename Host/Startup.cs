f345fa3a2d026a1732bb7adfe5dc2f6522a2bd59
jerome <jeromestpierre@gmail.com>
2020-03-07 17:13:54 +0000

    Integrate Swagger UI

--- Host/Startup.cs
+++ Host/Startup.cs
@@ -1,7 +1,9 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Shakespokemon.DataAccess;
 using Shakespokemon.Core;
 
@@ -11,12 +13,27 @@ namespace Shakespokemon.Host
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo { Title = "Shakespokemon API" });
+
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, "Shakespokemon.Host.XML");
+                config.IncludeXmlComments(xmlPath);
+            });
+
             services.AddControllers();
             services.AddTransient<IPokemonRepository, PokemonRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shakespokemon API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
