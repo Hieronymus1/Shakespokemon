3864203daccb7335b01f189c66dbb78da7e4a040
jerome <jeromestpierre@gmail.com>
2020-03-08 20:50:33 +0000

    Add missing project reference and dependencies

--- Etl/Console/Program.cs
+++ Etl/Console/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Shakespokemon.Etl.Core;
 using Shakespokemon.Etl.DataAccess;
 using Shakespokemon.Etl.DataAccess.Http;
+using Shakespokemon.Api.DataAccess;
 
 namespace Shakespokemon.Etl.Console
 {
@@ -15,6 +16,8 @@ namespace Shakespokemon.Etl.Console
                 .AddSingleton<IPokemonNamesClient, PokeApiClient>()
                 .AddSingleton<IPokemonDescriptionClient, PokedexClient>()
                 .AddSingleton<IShakespeareTranslationClient, FunTranslationClient>()
+                .AddSingleton<IPokemonSourceRepository, PokemonSourceRepository>()
+                .AddSingleton<IPokemonDestinationRepository, PokemonRepository>()
                 .AddSingleton<IPokemonLoadService, PokemonLoadService>()
                 .AddLogging()
                 .BuildServiceProvider();
