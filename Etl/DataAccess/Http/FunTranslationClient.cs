68aeb1223ec7acd332d4005c11353dd0c870418e
jerome <jeromestpierre@gmail.com>
2020-03-08 19:02:26 +0000

    Test FunTranslationClient HTTP POST

--- Etl/DataAccess/Http/FunTranslationClient.cs
+++ Etl/DataAccess/Http/FunTranslationClient.cs
@@ -1,15 +1,28 @@
 using System;
 using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Collections.Generic;
 
 namespace Shakespokemon.Etl.DataAccess.Http
 {
     internal class FunTranslationClient : IShakespeareTranslationClient
     {
+        private readonly Uri Uri = new Uri("https://api.funtranslations.com/translate/shakespeare.json");
+
         public string GetTranslation(string text)
         {
-            // Post text for translation: https://api.funtranslations.com/translate/shakespeare.json
+            using(var client = new HttpClient())
+            {
+                var textContent = new KeyValuePair<string, string>("text", text);
+                using(var content = new FormUrlEncodedContent(new[] { textContent }))
+                {
+                    var response = client.PostAsync(Uri, content).Result;  
+                    response.EnsureSuccessStatusCode();  
+                    var json = response.Content.ReadAsStringAsync().Result;  
 
-            throw new NotImplementedException();
+                    return ParseTranslation(json);
+                }
+            }
         }
 
         public static string ParseTranslation(string json)
--- Etl/Tests/DataAccess/Http/FunTranslationClientTests.cs
+++ Etl/Tests/DataAccess/Http/FunTranslationClientTests.cs
@@ -17,5 +17,17 @@ namespace Shakespokemon.Etl.Tests.DataAccess.Http
             Assert.IsNotNull(actual);
             Assert.AreEqual(expected, actual);
         }
+
+        [Test, Ignore("Avoid API calls limit by IP")]
+        public void GivenText_WhenGetDescription_ThenReturnsTranslation()
+        {
+            var sut = new FunTranslationClient();
+            var expected = "Charizard flies 'round the sky in search of powerful opponents. 't breathes fire of such most wondrous heat yond 't melts aught. However,  't nev'r turns its fiery breath on any opponentweaker than itself.";
+
+            var text = "Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponentweaker than itself.";
+            var actual = sut.GetTranslation(text);
+
+            Assert.AreEqual(actual, expected);
+        }
     }
 }
